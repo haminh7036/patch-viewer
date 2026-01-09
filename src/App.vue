e57bb4c34fce62aa5d79069ad3221c2f184b8fd2
MinhHa <minhha7036@gmail.com>
2026-01-10 00:11:31 +0700

    update label

--- src/App.vue
+++ src/App.vue
@@ -86,8 +86,8 @@
           <div
             class="border-2 border-dashed border-gray-300 rounded-xl p-6 md:p-10 flex flex-col items-center max-w-lg w-full bg-white">
             <FileDiff class="w-12 h-12 mb-4 text-gray-300" />
-            <h3 class="text-base font-semibold text-gray-600 text-center">Upload File .patch / .diff</h3>
-            <p class="text-xs text-gray-400 mb-4 text-center">Or Paste Content Into This Below</p>
+            <h3 class="text-base font-semibold text-gray-600 text-center">Upload file .patch / .diff</h3>
+            <p class="text-xs text-gray-400 mb-4 text-center">Or paste content into this textarea below</p>
             <textarea v-model="rawInput" @input="debouncedPaste" placeholder="Diff content here..."
               class="w-full h-32 p-3 text-xs bg-gray-50 border rounded resize-none focus:ring-1 focus:ring-blue-500 outline-none code-font"></textarea>
           </div>
